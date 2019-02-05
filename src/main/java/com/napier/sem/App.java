b0ed18dca0b805bcaa2602a2777ef8c20fc3bade
Alfie Thomasson <alfiethomasson0@gmail.com>
2019-02-05 19:46:17 +0000

    Adding Zube Stuff

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -173,4 +173,18 @@ public class App
             return null;
         }
     }
+
+    public void printSalaries(ArrayList<Employee> employees)
+    {
+        // Print header
+        System.out.println(String.format("%-10s %-15s %-20s %-8s", "Emp No", "First Name", "Last Name", "Salary"));
+        // Loop over all employees in the list
+        for (Employee emp : employees)
+        {
+            String emp_string =
+                    String.format("%-10s %-15s %-20s %-8s",
+                            emp.emp_no, emp.first_name, emp.last_name, emp.salary);
+            System.out.println(emp_string);
+        }
+    }
 }
